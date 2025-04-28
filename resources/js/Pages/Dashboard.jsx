705a1c070ac961d18b9c09779c1ddc193958aea2
darren <darren.kooyz@gmail.com>
2025-04-28 17:22:15 +0800

    feat: ant design

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,7 +1,19 @@
+import { ChevronDown } from '@/Components/Icon/Outline';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
+import { Select, Tag } from 'antd';
 
 export default function Dashboard() {
+
+    const options = [
+        { value: '1', label: 'Jack' },
+        { value: '2', label: 'Lucy' },
+    ]
+
+    const handleChange = (value) => {
+        console.log(value);
+    }
+
     return (
         <AuthenticatedLayout
             header={
@@ -16,7 +28,15 @@ export default function Dashboard() {
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
-                            You're logged in!
+                        <Select
+                            onChange={handleChange}
+                            options={options}
+                            mode="multiple"
+                            placeholder="Placeholder"
+                            suffixIcon={<ChevronDown />}
+                        />
+                        <Tag bordered={false} color="success" className='text-xs font-medium py-1 px-2 font-sans' >Placeholder</Tag>
+                        <Tag bordered={false} color="warning" className=' text-xs font-medium py-1 px-2 font-sans' >Placeholder</Tag>
                         </div>
                     </div>
                 </div>
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,6 +4,7 @@ import './bootstrap';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
+import { ConfigProvider } from 'antd';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -17,7 +18,45 @@ createInertiaApp({
     setup({ el, App, props }) {
         const root = createRoot(el);
 
-        root.render(<App {...props} />);
+        root.render(
+            <ConfigProvider
+                theme={{
+                    cssVar: true,
+                    hashed: false,
+                    token: {
+                        colorPrimary: '#030712', // 使用 Tailwind 的 blue-500
+                        borderRadius: 8,
+                    },
+                    components: {
+                        Select: {
+                            colorBorder: '#D1D5DB', // Tailwind gray-200
+                            colorPrimaryHover: '#9CA3AF', // Tailwind blue-500
+                            activeBorderColor: '#030712',
+                            optionSelectedBg: '#eff6ff', // Tailwind blue-50
+                            optionSelectedColor: '#2563eb', // Tailwind blue-600
+                            colorText: '#030712',
+                            fontSize: '14px',
+                            lineHeight: '20px',
+                            fontFamily: 'Outfit, sans-serif',
+                            optionFontSize: '14px',
+                            optionLineHeight: '20px',
+                            optionPadding: '8px 16px',
+                            optionSelectedBg: '#FFF',
+                            optionSelectedColor: '#030712',
+                            optionSelectedFontWeight: '400',
+                            selectorBg: '#FFF', // Tailwind gray-100
+                            multipleItemHeight: '20px'
+                        },
+                        Tag: {
+                            defaultBg: '#F3F4F6',
+                            defaultColor: '#6B7280'
+                        }
+                    }
+                }}
+            >
+                <App {...props} />
+            </ConfigProvider>
+    );
     },
     progress: {
         color: '#4B5563',
